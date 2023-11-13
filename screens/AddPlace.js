b34c13ae28b852acc7220e3379bb5b60ff3b84dc
Rishi Pramanik <rishi.pra95@gmail.com>
2023-11-13 18:00:20 +0530

    added base screens and nav

---
+++ screens/AddPlace.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PlaceForm from '../components/Places/PlaceForm';
+
+function AddPlace() {
+  return <PlaceForm />;
+}
+
+export default AddPlace;
---
+++ screens/AllPlaces.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PlacesList from '../components/Places/PlacesList';
+
+function AllPlaces() {
+  return <PlacesList />;
+}
+
+export default AllPlaces;
